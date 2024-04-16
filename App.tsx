c7cf322cde5b2c7efe64aa35bcb771847744a043
Katayoon Faraji <144775981+katayoon-faraji-web@users.noreply.github.com>
2024-04-16 16:37:22 +0330

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Form1 from './components/form1';
+import List from './components/list';
+import Form2 from './components/form2'
+
+function App() {
+  
+  return (
+    <main style={{width:'100%' ,display:'flex',height:'100vh',flexWrap:'wrap',justifyContent:'center'}}>
+      {/* <Form1/>
+      <List /> */}
+      <Form2 />
+
+    </main>
+  )
+}
+
+export default App
